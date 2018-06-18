1e35e758945d48e34b3cc6f491598abcaf2c72b2
dave ciaccio <github@gravyday.com>
2018-06-17 22:41:54 -0700

    Initial Commit

---
+++ LarkStTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LarkStTest
+//
+//  Created by dave on 6/17/18.
+//  Copyright © 2018 dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
